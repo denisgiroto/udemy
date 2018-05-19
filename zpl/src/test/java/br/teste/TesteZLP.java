91363e2f01d52c8409839f6885127b7109e3b28e
denisgiroto <denisgiroto@gmail.com>
2018-05-19 03:43:40 -0300

    ZPL printer

--- zpl/src/test/java/br/teste/TesteZLP.java
+++ zpl/src/test/java/br/teste/TesteZLP.java
@@ -12,9 +12,8 @@ import br.com.dsg.zpl.elemento.Fonte;
 import br.com.dsg.zpl.elemento.Posicao;
 import junit.framework.Assert;
 
-
 public class TesteZLP {
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -31,56 +30,50 @@ public class TesteZLP {
 	public void tearDown() throws Exception {
 	}
 
-
-
 	@Test
 	public void testPagina() {
-		String valor = ZPL
-		.novaPagina(12, 70)
-		.escrever();
-		
+		String valor = ZPL.novaPagina(12, 70)
+				.escrever();
+
 		System.out.println(valor);
 		Assert.assertEquals(valor, "^XA^LL560^XZ");
 	}
-	
+
 	@Test
 	public void testFonte() {
-		String valor = ZPL
-			.novaPagina(12, 70)
+		String valor = ZPL.novaPagina(12, 70)
 				.registra(new Fonte(12))
-			.escrever();
-		
+				.escrever();
+
 		System.out.println(valor);
 		Assert.assertEquals(valor, "^XA^LL560^CF0,96^XZ");
-		
+
 	}
-	
+
 	@Test
 	public void testAlfa() {
-		String valor = ZPL
-			.novaPagina(12, 70)
-				.registra(new CampoAlfanumerico(new Posicao(10, 10) , "Denis Giroto").comRotacao("A", "I") )
-			.escrever();
-		
+		String valor = ZPL.novaPagina(12, 70)
+				.registra(new CampoAlfanumerico(new Posicao(10, 10), "Denis Giroto")
+						.comRotacao("A", "I"))
+				.escrever();
+
 		System.out.println(valor);
 		Assert.assertEquals(valor, "^XA^LL560^FO80,80^AAI^FDDenis Giroto^FS^XZ");
-		
+
 	}
-	
+
 	@Test
 	public void testFonteComAlfa() {
-		String valor = ZPL
-				.novaPagina(12, 70)
+		String valor = ZPL.novaPagina(12, 70)
 				.registra(new Fonte(12))
-				.registra(new CampoAlfanumerico(new Posicao(10, 10) , "Denis Giroto").comRotacao("A", "I") )
+				.registra(new CampoAlfanumerico(new Posicao(10, 10), "Denis Giroto")
+						.comRotacao("A", "I")
+						.comDimensao("10", "10"))
+						
 				.escrever();
-		
-		System.out.println(valor);
-		Assert.assertEquals(valor, "^XA^LL560^CF0,96^FO80,80^AAI^FDDenis Giroto^FS^XZ");
-	}
-	
-	
-	
 
+		System.out.println(valor);
+		Assert.assertEquals(valor, "^XA^LL560^CF0,96^FO80,80^AAI,80,80^FDDenis Giroto^FS^XZ");
+	}
 
 }
